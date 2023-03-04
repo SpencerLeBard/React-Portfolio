10b39c20b5e750eaeb409dbd2c71c779f94ea202
SpencerLeBard <slebard@gmail.com>
2023-03-04 11:57:26 -0700

    nav changes

--- src/client/Components/Navigation.js
+++ src/client/Components/Navigation.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 function Navigation() {
   return (
 <div className="Navigation">
-    <div className="container">
+    <div className="container font-mono">
       <div className="row">
         <div className="col-3">
             Spencer's Blog and Portfolio
         </div>
-        <div className="col-4 ">
-        <Link to="/Portfolio">Portfolio</Link>
+        <div className="col-4 decoration-black">
+        <Link style={{textDecoration: 'none'}} to="/Portfolio">Portfolio</Link>
         </div>
       </div>
     </div>
--- src/client/pages/Portfolio.js
+++ src/client/pages/Portfolio.js
@@ -3,8 +3,8 @@ import './Portfolio.css'
 
 function Portfolio() {
   return (
-<div className="Portfolio text-3xl font-bold underline">
-    <h1 className="text-3xl font-bold underline" >Portfolio</h1>
+<div className="Portfolio dark:md:hover:bg-fuchsia-600">
+    <h1 className="text-3xl " >Portfolio</h1>
  </div>
   );
 }
