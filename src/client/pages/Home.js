86e3ab48cd3dfaf1fbbc4b3114977e7b38a7e505
SpencerLeBard <slebard@gmail.com>
2023-03-06 10:12:41 -0700

    text showing

--- src/client/pages/Home.js
+++ src/client/pages/Home.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import backgroundArt from '../Assets/abstract.jpg'; 
+import marble from '../Assets/marble.jpg';
 
 
 function Home() {
   return (
-<div className="Home">
-  <div className="container">
-          <img  src={backgroundArt} alt=" art"/>
-        </div>
- </div>
+<div class="BackgroundHome">
+  <div className="relative">
+      <img
+        className="w-full max-h-screen w-screen"
+        src={backgroundArt}
+        alt="Background"
+      />
+      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+        <h1 className="text-white text-4xl font-bold">Hello, World!</h1>
+      </div>
+    </div>
+</div>
   );
 }
+
+
 
 export default Home;
