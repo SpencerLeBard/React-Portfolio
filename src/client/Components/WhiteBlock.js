aec9d5d2e8660b85e59ff5dc0499a6112b150d10
SpencerLeBard <slebard@gmail.com>
2023-03-07 14:27:29 -0700

    home matches figma

--- src/client/Components/WhiteBlock.js
+++ src/client/Components/WhiteBlock.js
@@ -8,15 +8,15 @@ function WhiteBlock() {
    <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
     <div className="bg-white w-75 h-75 rounded-2xl">
       {/* left side text */}
-      <div className="relative w-full h-full">
+      <div className="relative">
           <h1 className="absolute left-5 top-5 text-black text-4xl font-bold font-mono">Spencer LeBard</h1>
       {/* TODO RIGHT side text */}
-          <div className="">
+          <div className="absolute right-5 top-5">
           <h1 className="text-black text-4xl font-bold font-mono">Home</h1>
           <h1 className="text-black text-4xl font-bold font-mono">Portfolio</h1>
-          <Link to="/BlogHome">
+          <Link style={{textDecoration: 'none'}} to="/BlogHome">
           <h1 className="text-black text-4xl font-bold font-mono">Blog</h1></Link>
-          <h1 className="text-black text-4xl font-bold font-mono">Resume</h1>
+          <h1 className="text-black text-4xl font-bold font-mono">Contact Me</h1>
           </div>
       </div>
       </div>
