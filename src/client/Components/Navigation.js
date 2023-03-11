87fbcabd3f7976b3f47858dc314b1a3fbe3dc063
SpencerLeBard <slebard@gmail.com>
2023-03-10 17:12:30 -0700

    started firstblog

--- src/client/Components/Navigation.js
+++ src/client/Components/Navigation.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 function Navigation() {
   return (
-<div className="Navigation">
-    <div className="container font-mono">
+<div className="Navigation  h-7 bg-marble">
+    <div className="container font-mono ">
       <div className="row">
         <div className="col-3">
             Spencer's Blog and Portfolio
         </div>
         <div className="col-4 decoration-black">
-        <Link style={{textDecoration: 'none'}} to="/Portfolio">Portfolio</Link>
+        <Link style={{textDecoration: 'none'}} to="/BlogHome"> Blog Home </Link>
         </div>
       </div>
     </div>
--- src/client/pages/FirstBlog.js
+++ src/client/pages/FirstBlog.js
@@ -1,62 +1,22 @@
-import React from 'react'
-import './FirstBlog.css'
+import React from 'react';
+import Navigation from '../components/Navigation';
 
 function FirstBlog() {
   return (
 <div className="FirstBlog">
-  <article class="blog-post">
-        <h2 class="blog-post-title mb-1">Sample blog post</h2>
-        <p class="blog-post-meta">January 1, 2021 by <a href="www.google.com">Mark</a></p>
-
-        <p>This blog post shows a few different types of content that’s supported and styled with Bootstrap. Basic typography, lists, tables, images, code, and more are all supported as expected.</p>
-        <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-        <h2>Blockquotes</h2>
-        <p>This is an example blockquote in action:</p>
-        <blockquote class="blockquote">
-          <p>Quoted text goes here.</p>
-        </blockquote>
-        <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-        <h3>Example lists</h3>
-        <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout. This is an example unordered list:</p>
-        <ul>
-          <li>First list item</li>
-          <li>Second list item with a longer description</li>
-          <li>Third list item to close it out</li>
-        </ul>
-        <p>And this is an ordered list:</p>
-        <ol>
-          <li>First list item</li>
-          <li>Second list item with a longer description</li>
-          <li>Third list item to close it out</li>
-        </ol>
-        <p>And this is a definition list:</p>
-        <dl>
-          <dt>HyperText Markup Language (HTML)</dt>
-          <dd>The language used to describe and define the content of a Web page</dd>
-          <dt>Cascading Style Sheets (CSS)</dt>
-          <dd>Used to describe the appearance of Web content</dd>
-          <dt>JavaScript (JS)</dt>
-          <dd>The programming language used to build advanced Web sites and applications</dd>
-        </dl>
-        <h2>Inline HTML elements</h2>
-        <p>HTML defines a long list of available inline tags, a complete list of which can be found on the <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element">Mozilla Developer Network</a>.</p>
-        <ul>
-          <li><strong>To bold text</strong>, use <code class="language-plaintext highlighter-rouge">&lt;strong&gt;</code>.</li>
-          <li><em>To italicize text</em>, use <code class="language-plaintext highlighter-rouge">&lt;em&gt;</code>.</li>
-          <li>Abbreviations, like <abbr title="HyperText Markup Language">HTML</abbr> should use <code class="language-plaintext highlighter-rouge">&lt;abbr&gt;</code>, with an optional <code class="language-plaintext highlighter-rouge">title</code> attribute for the full phrase.</li>
-          <li>Citations, like <cite>— Mark Otto</cite>, should use <code class="language-plaintext highlighter-rouge">&lt;cite&gt;</code>.</li>
-          <li><del>Deleted</del> text should use <code class="language-plaintext highlighter-rouge">&lt;del&gt;</code> and <ins>inserted</ins> text should use <code class="language-plaintext highlighter-rouge">&lt;ins&gt;</code>.</li>
-          <li>Superscript <sup>text</sup> uses <code class="language-plaintext highlighter-rouge">&lt;sup&gt;</code> and subscript <sub>text</sub> uses <code class="language-plaintext highlighter-rouge">&lt;sub&gt;</code>.</li>
-        </ul>
-        <p>Most of these elements are styled by browsers with few modifications on our part.</p>
-        <h2>Heading</h2>
-        <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-        <h3>Sub-heading</h3>
-        <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-        <pre><code>Example code block</code></pre>
-        <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout.</p>
-      </article>
- </div>
+<Navigation />
+<div className="TopHalf relative h-32 w-32 ">
+  <div className="LeftSide absolute left-0 top-0 h-16 w-16bsolute left-0 top-0 h-16 w-16">
+    <h1> Featured Blog Post Title</h1>
+    <h4>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups. </h4>
+    <h4> By: Spencer LeBard</h4>
+    <h4> LinkedinBox|GithubBox|TwitterBox|OtherSocalBox</h4>
+    </div>
+    <div className="absolute top-0 left-0 right-0 bottom-0 flex items-end justify-end">
+    <div className="bg-white w-75 h-75 rounded-2xl">picture</div>
+    </div>
+  </div>  
+</div>
   );
 }
 
