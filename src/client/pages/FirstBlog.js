f4f00d37bd94f25c6b769c0ab1f31f51e59c0cbd
SpencerLeBard <slebard@gmail.com>
2023-03-10 18:49:56 -0700

    got major parts

--- src/client/pages/FirstBlog.js
+++ src/client/pages/FirstBlog.js
@@ -5,17 +5,17 @@ function FirstBlog() {
   return (
 <div className="FirstBlog">
 <Navigation />
-<div className="TopHalf relative h-32 w-32 ">
-  <div className="LeftSide absolute left-0 top-0 h-16 w-16bsolute left-0 top-0 h-16 w-16">
+<div className="TopHalf row relative">
+  <div className="LeftSide absolute left-0 top-0 col-3">
     <h1> Featured Blog Post Title</h1>
-    <h4>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups. </h4>
+    <h6>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups. </h6>
     <h4> By: Spencer LeBard</h4>
     <h4> LinkedinBox|GithubBox|TwitterBox|OtherSocalBox</h4>
     </div>
-    <div className="absolute top-0 left-0 right-0 bottom-0 flex items-end justify-end">
-    <div className="bg-white w-75 h-75 rounded-2xl">picture</div>
-    </div>
+    <div className="absolute bg-black w-64 h-64 rounded-2 text-white right-0 top-0 col-3"> picture </div>
   </div>  
+  <div className="BottomHalf absolute bottom-10 outline-5 outline-red outline-double row">
+  <h6>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups. </h6> </div>
 </div>
   );
 }
