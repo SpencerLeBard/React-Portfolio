42b84442753db19559fe0beecdd4ca08f3cf39c3
Spencer LeBard <Slebard@gmail.com>
2025-04-09 19:21:13 -0600

    blog

--- src/client/Components/BlogData.js
+++ src/client/Components/BlogData.js
@@ -1,48 +1,48 @@
 const BlogData = [
-    {
-      id: 1,
-      title: "Client Cried Wolf",
-      date: "April 2, 2025",
-      content: `
-        Story about a client who cried wolf. 
-        consultant did their best but left the client and the client told
-        everyone that the consultant sucked ... [look at DB for more]
-      `,
-    },
+    // {
+    //   id: 1,
+    //   title: "Client Cried Wolf",
+    //   date: "April 2, 2025",
+    //   content: `
+    //     Story about a client who cried wolf. 
+    //     consultant did their best but left the client and the client told
+    //     everyone that the consultant sucked ... [look at DB for more]
+    //   `,
+    // },
     {
       id: 2,
       title: "Consultants are Service Workers",
-      date: "April 3, 2025",
+      date: "April 10, 2025",
       content: `
         high paying consulting is the same as fast food.  whenever money is 
         exchanged for a service.  even millionare MDs get yelled at when their order
         is wrong.
       `,
-    },
-    {
-      id: 3,
-      title: "Corps are full of selfish people",
-      date: "April 12, 2023",
-      content: `
-        businesses are full of people who only care about themselves...
-      `,
-    },
-    {
-      id: 4,
-      title: "Flip the script: Short Story",
-      date: "April 12, 2023",
-      content: `
-        cowboys move to SF and Seattle and locals fightback and are not tellerant.
-      `,
-    },
-    {
-        id: 5,
-        title: "Salesforce developer: Day in the life",
-        date: "April 12, 2023",
-        content: `
-          How to be a salesforce dev.
-        `,
-      }
+     }
+    // {
+    //   id: 3,
+    //   title: "Corps are full of selfish people",
+    //   date: "April 12, 2023",
+    //   content: `
+    //     businesses are full of people who only care about themselves...
+    //   `,
+    // },
+    // {
+    //   id: 4,
+    //   title: "Flip the script: Short Story",
+    //   date: "April 12, 2023",
+    //   content: `
+    //     cowboys move to SF and Seattle and locals fightback and are not tellerant.
+    //   `,
+    // },
+    // {
+    //     id: 5,
+    //     title: "Salesforce developer: Day in the life",
+    //     date: "April 12, 2023",
+    //     content: `
+    //       How to be a salesforce dev.
+    //     `,
+    //   }
       //NOTE: Look at microsoft word
   ];
   
