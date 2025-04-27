4cdf757c934cffe056ed3585bd4064b6b57485fd
Spencer LeBard <Slebard@gmail.com>
2025-04-27 15:08:42 -0600

    streamline link live

--- src/client/pages/Portfolio.js
+++ src/client/pages/Portfolio.js
@@ -12,7 +12,7 @@ const Portfolio = () => {
       "Modern video hosting and streaming platform built with React, Node/Express, and MongoDB",
       imageUrl: streamlinelogo,
       link: "https://github.com/SpencerLeBard/Streamline",
-      liveLink: "https://streamline-app.example.com"
+      liveLink: "https://streamline-qxilndw7n-spencer-lebards-projects.vercel.app/"
     },
     {
       title: "PetPal",
